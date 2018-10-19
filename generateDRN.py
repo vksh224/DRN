33790a7f28a19f904e838cc868bad99a99a7579a
vksh224 <vijay.shah@uky.edu>
2018-10-19 14:55:22 -0400

    changed the generate DRN file to match realistic setting

--- generateDRN.py
+++ generateDRN.py
@@ -20,13 +20,25 @@ def generate(X,Y,VN,R):
     # Number of hub, sub and non
     HC = int(0.01 * VN)
     SC = int(0.05 * VN)
-    NC = VN - HC - SC
+    #NC = VN - HC - SC
 
     # Set of hub, sub and non-hubs
 
     HCN = N[:HC]
     SCN = N[HC:(HC + SC)]
     NCN = N[(HC + SC):]
+
+    #CC location
+    for i in HCN:
+        C[i] = (random.randint(0,X),random.randint(0,Y))
+
+    #PoI location
+    for i in SCN:
+        C[i] = (random.randint(0,X),random.randint(0,Y))
+
+    #Survivor location
+    for i in NCN:
+        C[i] = (random.randint(max(C[i][0]  - 10, 0), min(X, C[i][0] + poi_radius)),random.randint(max(C[i][0]  - 10, 0), min(Y, C[i][0] + poi_radius)))
 
     '''
     while(len(N) > 0):
@@ -50,11 +62,37 @@ def generate(X,Y,VN,R):
     # Introduce edges
     for u in G.nodes():
         for v in G.nodes():
-            if u != v and euclidean(C[u],C[v]) <= R:
-                G.add_edge(u,v)
+            #CC and CC
+            if u!= v and u in HCN and v in HCN and euclidean(C[u],C[v]) <= tower_range:
+                G.add_edge(u, v)
+                # print("C - C", u, v, HCN)
+
+            # CC and PoI
+            if u != v and u in HCN and v in SCN and euclidean(C[u], C[v]) <= tower_range:
+                G.add_edge(u, v)
+                # print("C - P", u, v, HCN)
+
+            # PoI and PoI
+            if u != v and u in SCN and v in SCN and euclidean(C[u], C[v]) <= tower_range:
+                G.add_edge(u, v)
+                # print("P - P", u, v, HCN)
+
+            # PoI and Survivor
+            if u != v and u in SCN and v in NCN and euclidean(C[u], C[v]) <= ble_range:
+                G.add_edge(u, v)
+                # print("P - S", u, v, HCN)
+
+            # Survivor and Survivor
+            if u != v and u in NCN and v in NCN and euclidean(C[u], C[v]) <= ble_range:
+                G.add_edge(u, v)
+                # print("S - S", u, v, HCN)
+
+            # if u != v and euclidean(C[u],C[v]) <= R:
+            #     G.add_edge(u,v)
 
     print (len(G))
     print (len(G.edges()))
+    print ((len(G.edges()) * 2)/ (len(G) * (len(G) - 1)))
 
     return G, HCN, SCN, NCN
 
@@ -63,6 +101,10 @@ X = 50
 Y = 50
 # Communication Radius
 R = 20
+
+tower_range = 20
+ble_range = 10
+poi_radius = 10
 # Number of nodes in input DRN
 # VN = 100
 curr = os.getcwd()
