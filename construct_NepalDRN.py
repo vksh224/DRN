75993aa21a10d3a130e8d887ffdef7dfd047ca1e
Vijay <vjsah27@gmail.com>
2018-11-01 10:23:38 -0400

    nepal DRN changes

--- construct_NepalDRN.py
+++ construct_NepalDRN.py
@@ -3,10 +3,12 @@ import math
 import networkx as nx
 from scipy.spatial.distance import *
 from writeFile import *
+#from turtle1 import *
 
 #allocate each survivor to a unique PoI
 def allocate_survivors():
     last_S_id = 0
+
     # PoI location
     for i in range(len(PoIs_loc)):
        #allocate each survivor in this PoI
@@ -20,26 +22,26 @@ def allocate_survivors():
             y = r * math.sin(alpha) + PoIs_loc[i][1]
             S_loc[j + last_S_id] = (x, y)
 
-        prev_S_id = last_S_id
+        #prev_S_id = last_S_id
         last_S_id += PoI_S_count[i]
+
         # print("PoI ", i,  PoIs_loc[i])
         # print("Survivors", PoI_S_count[i], [S_loc[j] for j in range(prev_S_id, last_S_id)])
-
 
 #Get links between tier 1 and tier 2 nodes
 def get_tier1_tier2_links(G):
     for res in responders:
         for u in res:
             for v in res:
-                if u != v:
+                if u != v and G.has_edge(u, v) == False:
                     G.add_edge(u, v)
                     #G.add_edge(v, u)
 
 
 def generate_Nepal_DRN():
-    time_periods = 13
-    loc_o = '0 ' + str(time_periods * 600) + " 0 " + str(X) + " 0 " + str(Y) + '\n'
-    nei_o = '0 ' + str(time_periods * 600) + '\n'
+    time_periods = 720
+    loc_o = '0 ' + str(time_periods * 300) + " 0 " + str(X) + " 0 " + str(Y) + '\n'
+    nei_o = '0 ' + str(time_periods * 300) + '\n'
 
     for i in range(time_periods):
         G = nx.DiGraph()
@@ -58,7 +60,6 @@ def generate_Nepal_DRN():
             PoI_IDs.append(node_id)
             node_id += 1
 
-
         #add survivors
         for s in S_loc:
             G.add_node(node_id)
@@ -73,8 +74,8 @@ def generate_Nepal_DRN():
         for u in G.nodes():
             #print(u, node_locs[u])
             for v in G.nodes():
-                    if u != v and euclidean(node_locs[u], node_locs[v]) <= bt_range:
-                        G.add_edge(u,v)
+                if u != v and euclidean(node_locs[u], node_locs[v]) <= bt_range:
+                    G.add_edge(u,v)
 
         loc_o += writeC(node_locs, i)
         nei_o += writeF(G, i)
@@ -87,23 +88,23 @@ def generate_Nepal_DRN():
 
 
 #Main starts here
+X = 10000
+Y = 10000
 
-X = 5000
-Y = 5000
-
-V = 200
+#V = 200 # survivor only
 poi_radius = 200
 
 bt_range = 100
 tower_range = 1000
 
-CC_loc = [(2809.0, 4516.0)]
-PoIs_loc = [(2190.0, 5390.0), (2370.0, 4530.0), (2940.0, 5240.0), (3690.0, 4160.0), (4230.0, 4700.0), (4620.0, 4990.0)]
+CC_loc = [(2809, 4516)]
+PoIs_loc = [(2190, 5390), (2370, 4530), (2940, 5240), (3690, 4160), (4230, 4700), (4620, 4990)]
+responders = [[0, 2, 4, 0], [0, 3, 2, 0], [0, 3, 1, 3, 0], [0, 4, 0], [0, 5, 6, 0], [0, 6, 0], [0, 6, 5, 4, 0], [0, 5, 0]]
 
 PoI_S_count = [25, 35, 35, 45, 35, 25]
-S_loc = [(-1, -1) for i in range(V)]
-responders = [[0, 2, 4, 0], [0, 3, 2, 0]]
-
+PoI_Vol_count = [2, 2, 3, 3, 3, 2]
+S_loc = [(-1, -1) for i in range(sum(PoI_S_count))]
+Vol_loc = [(-1, -1) for i in range(sum(PoI_Vol_count))]
 
 CC_0 = 0
 CC_n = len(CC_loc)
@@ -112,13 +113,14 @@ PoIs_0 = CC_0
 PoIs_n = PoIs_0 + len(PoIs_loc)
 
 S_0 = PoIs_n
-S_n = S_0 + V
+S_n = S_0 + sum(PoI_S_count)
 
+#all cc, poi, S ids
 CC_IDs = []
 PoI_IDs = []
 S_IDs = []
-
 node_locs = []
+
 allocate_survivors()
 
 for loc in CC_loc:
@@ -130,17 +132,16 @@ for loc in PoIs_loc:
 for loc in S_loc:
     node_locs.append(loc)
 
-
 G, loc_o, nei_o = generate_Nepal_DRN()
 
 neigh_des_folder = "/Users/vijay/BioDRNICDCSWorkSpace/ONEICDCS/src/NeighborList/"
 loc_des_folder = "/Users/vijay/BioDRNICDCSWorkSpace/ONEICDCS/src/NodePosition/"
 
-f = open(loc_des_folder + 'O_C' + str(V) + '_new.txt','w')
+f = open(loc_des_folder + 'O_C' + str(sum(PoI_S_count)) + '_new.txt','w')
 f.write(loc_o)
 f.close()
 
-f = open(neigh_des_folder + 'O_N' + str(V) + '_new.txt','w')
+f = open(neigh_des_folder + 'O_N' + str(sum(PoI_S_count)) + '_new.txt','w')
 f.write(nei_o)
 f.close()
 
---
+++ turtle1.py
@@ -0,0 +1,84 @@
+import turtle
+import random
+
+
+def volunteer_path(poi_loc):
+    polygon = turtle.Turtle()
+
+    num_sides = random.randint(4, 10)
+    side_length = random.randint(10, poi_radius)
+    angle = 360.0 / num_sides
+
+    #polygon.speed(random.randint(1, 5))
+    polygon.setposition(poi_loc)
+
+    vol_coor_list = []
+    print("Number of sides: ", num_sides)
+    for i in range(num_sides):
+        #print(i, polygon.position())
+        vol_coor_list.append(str(polygon.position()))
+        polygon.forward(side_length)
+        polygon.right(angle)
+    #turtle.done()
+    return vol_coor_list
+
+def allocate_volunteers():
+
+    f_vol = open(directory + 'volunteer_paths.txt', 'w')
+    vol_count = len(responders) + 1
+    for poi_id in range(len(PoIs_loc)):
+        for vol_id in range(PoI_Vol_count[poi_id]):
+            f_vol.write("Group.waypoints" + str(vol_count) + " = ")
+
+            vol_coor_list = volunteer_path(PoIs_loc[poi_id])
+            with open(directory + "poi_" + str(poi_id) + "vol_" + str(vol_id) + '.txt', 'w') as f:
+                for coor in vol_coor_list:
+                    coor = coor.strip("()").split(",")
+                    print ("coor: ", poi_id, vol_id, str(coor))
+                    f.write(str(coor[0]) + " " + str(coor[1]) + "\n")
+                    f_vol.write(str(coor[0]) + ", " + str(coor[1]) + ", ")
+
+            vol_count += 1
+            f_vol.write("\n")
+            f.close()
+    f_vol.close()
+
+
+def allocate_responders():
+    CC_PoI_locs = []
+
+    #CC and PoI locations
+    for loc in CC_loc:
+        CC_PoI_locs.append(loc)
+    for loc in PoIs_loc:
+        CC_PoI_locs.append(loc)
+
+    f = open(directory + 'responder_paths.txt', 'w')
+
+    path_count = 1
+    for res_path in responders:
+        #print("Res : ", res_path, " ")
+        line_str = "Group.waypoints" + str(path_count) +" = "
+
+        for node_id in res_path:
+            #print("Here", CC_PoI_locs[node_id], CC_PoI_locs[node_id][0], str(CC_PoI_locs[node_id][1]), "there")
+            line_str += str(CC_PoI_locs[node_id][0]) + ", " + str(CC_PoI_locs[node_id][1]) + " , "
+        f.write(line_str + "\n")
+        path_count += 1
+    f.close()
+
+#Main starts here
+poi_radius = 200
+
+CC_loc = [(2809, 4516)]
+PoIs_loc = [(2190, 5390), (2370, 4530), (2940, 5240), (3690, 4160), (4230, 4700), (4620, 4990)]
+PoI_Vol_count = [2, 2, 3, 3, 3, 2]
+responders = [[0, 2, 4, 0], [0, 3, 2, 0], [0, 3, 1, 3, 0], [0, 4, 0], [0, 5, 6, 0], [0, 6, 0], [0, 6, 5, 4, 0], [0, 5, 0]]
+
+directory = "VolunteerPaths/"
+
+#Generate responder paths
+allocate_responders()
+
+#Generate volunteer paths
+allocate_volunteers()
